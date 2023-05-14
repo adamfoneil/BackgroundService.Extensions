2608ad300566a5a8aec8bd7dedde3b3d6ec4526d
Adam O'Neil <adamosoftware@gmail.com>
2023-05-14 19:57:45 -0400

    Add project files.

---
+++ BackgroundService.Extensions/CronJobBackgroundService.cs
@@ -0,0 +1,76 @@
+﻿using Cronos;
+using Sgbj.Cron;
+using System.Diagnostics;
+using Microsoft.Extensions.Hosting;
+
+namespace AO.HostedService.Extensions;
+
+public abstract class CronJobBackgroundService<T> : BackgroundService where T : IJobInfo, new()
+{
+	public CronJobBackgroundService()
+	{
+	}
+
+	public abstract string CrontabExpression { get; }
+
+	public abstract TimeZoneInfo TimeZone { get; }
+
+	public async Task ManualExecuteAsync(CancellationToken cancellationToken) => await ExecuteInnerAsync(cancellationToken, true);
+
+	protected abstract Task<(JobStatus Status, string Data)> DoWorkAsync(CancellationToken stoppingToken);
+
+	protected abstract Task UpdateJobInfoAsync(T jobInfo);
+
+	protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+	{
+		using var timer = new CronTimer(CrontabExpression, TimeZone);
+		
+		while (await timer.WaitForNextTickAsync(stoppingToken))
+		{
+			await ExecuteInnerAsync(stoppingToken, false);
+		}
+	}
+
+	protected virtual async Task LogInfoAsync(string level, string message, string detail) => await Task.CompletedTask;
+
+	private async Task ExecuteInnerAsync(CancellationToken stoppingToken, bool runningManually)
+	{
+		var jobInfo = new T() 
+		{ 
+			JobName = this.GetType().Name, 
+			Status = JobStatus.Running,
+			Started = new DateTimeOffset(DateTime.UtcNow, TimeZone.BaseUtcOffset),			
+			RunManually = runningManually
+		};
+
+		await UpdateJobInfoAsync(jobInfo);
+
+		var sw = Stopwatch.StartNew();
+		try
+		{								
+			var result = await DoWorkAsync(stoppingToken);						
+			jobInfo.Status = JobStatus.Succeeded;
+			jobInfo.Succeeded = new DateTimeOffset(DateTime.UtcNow, TimeZone.BaseUtcOffset);
+			jobInfo.ResultData = result.Data;			
+		}
+		catch (Exception exc)
+		{
+			jobInfo.Status = JobStatus.Failed;
+			jobInfo.Failed = new DateTimeOffset(DateTime.UtcNow, TimeZone.BaseUtcOffset);
+			jobInfo.ErrorMessage = exc.Message;
+		}
+		finally
+		{
+			sw.Stop();
+			jobInfo.Duration = TimeSpan.FromMilliseconds(sw.ElapsedMilliseconds);
+			jobInfo.NextOccurence = GetNextOccurrence();			
+			await UpdateJobInfoAsync(jobInfo);
+		}
+	}
+
+	private DateTime? GetNextOccurrence()
+	{
+		var expr = CronExpression.Parse(CrontabExpression);
+		return expr.GetNextOccurrence(DateTime.UtcNow, TimeZone);
+	}
+}
---
+++ BackgroundService.Extensions/Interfaces/IJobInfo.cs
@@ -0,0 +1,23 @@
+﻿namespace AO.HostedService.Extensions;
+
+public enum JobStatus
+{
+	Pending,
+	Running,
+	Succeeded,
+	Failed
+}
+
+public interface IJobInfo
+{
+	string JobName { get; set; }
+	JobStatus Status { get; set; }
+	bool RunManually { get; set; }
+	DateTimeOffset? NextOccurence { get; set; }
+	DateTimeOffset? Started { get; set; }
+	DateTimeOffset? Succeeded { get; set; }
+	string? ResultData { get; set; }
+	DateTimeOffset? Failed { get; set; }
+	string? ErrorMessage { get; set; }
+	TimeSpan? Duration { get; set; }
+}
