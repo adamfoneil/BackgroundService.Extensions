5013e71271c78d0d6324ca9bf773c0b13644c90c
Adam O'Neil <adamosoftware@gmail.com>
2023-10-09 09:04:16 -0400

    v1.0.5 ProcessQueue method #5

--- BackgroundService.Extensions/Extensions/DbConnectionExtensions.cs
+++ BackgroundService.Extensions/Extensions/DbConnectionExtensions.cs
@@ -5,6 +5,9 @@ namespace BackgroundServiceExtensions.Extensions;
 
 public static class DbConnectionExtensions
 {
+    /// <summary>
+    /// gets the next TMessage from a specified database table, ensuring that concurrent readers don't get the same row
+    /// </summary>
     public static async Task<(TMessage Message, bool Success)> DequeueAsync<TMessage>(this IDbConnection connection, string tableName, string? criteria = null, object? parameters = null)
     {
         string sql = $"DELETE TOP (1) FROM {tableName} WITH (ROWLOCK, READPAST) OUTPUT [deleted].*";
@@ -13,5 +16,18 @@ public static class DbConnectionExtensions
         TMessage message = await connection.QuerySingleOrDefaultAsync<TMessage>(sql, parameters);
 
         return (message, (message is not null));
+    }
+
+    /// <summary>
+    /// dequeue all TMessages from a table, doing some work on each TMessage
+    /// </summary>    
+    public static async Task ProcessQueueAsync<TMessage>(this IDbConnection connection, string tableName, Func<TMessage, Task> task, string? crtieria = null, object? parameters = null)
+    {        
+        do
+        {
+            var result = await DequeueAsync<TMessage>(connection, tableName, crtieria, parameters);
+            if (!result.Success) break;
+            await task.Invoke(result.Message);
+        } while (true);
     }
 }
